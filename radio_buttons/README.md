affe1ffcb709fa23fddef9668322f8b975681f57
Zong-han, Xie <icbm0926@gmail.com>
2016-01-17 18:36:46 +0800

    add radio_buttons exapmle

--- radio_buttons/README.md
+++ radio_buttons/README.md
@@ -1,1 +1,7 @@
 [GTK tutorial](https://developer.gnome.org/gtk-tutorial/stable/x542.html)
+
+[GTK+ How do I find which radio button is selected?](http://stackoverflow.com/questions/8812389/gtk-how-do-i-find-which-radio-button-is-selected)
+
+## Notes:
+* If I connect "find_which_button_in_group_activated" with "clicked" event, this function will be triggered twice. However,
+this problem can be solved through connection with "released" event.
--- radio_buttons/radio_buttons.c
+++ radio_buttons/radio_buttons.c
@@ -1,9 +1,16 @@
-include <stdlib.h>
-include <gtk/gtk.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
 
-static void click_callback(GtkWidget *widget, gpointer data)
-{
-    g_print("Hello again - %s was clicked.\n",(char *) data);
+static gboolean find_which_button_in_group_activated(GtkWidget *widget){
+    GSList * tmp_list = gtk_radio_button_get_group ((GtkRadioButton*) widget);
+    GtkRadioButton* tmp_button = NULL;
+    while(tmp_list){
+        tmp_button = tmp_list -> data;
+        if (gtk_toggle_button_get_active(tmp_button))
+            g_print("%s is activated.\n", gtk_button_get_label(tmp_button));
+        tmp_list = tmp_list -> next;
+    }
+    return TRUE;
 }
 
 static void click_callback(GtkWidget *widget, gpointer data)
@@ -18,7 +25,6 @@ static void press_callback(GtkWidget *widget, gpointer data)
 
 static void toggled_callback(GtkWidget *widget, gpointer data){
     g_print("Hello again - %s was toggled. ",(char *) data);
-    g_print("%s \n", gtk_toggle_button_get_mode((GtkToggleButton*)widget) ? "TRUE" : "FALSE");
 }
 
 static void release_callback(GtkWidget *widget, gpointer data)
@@ -58,6 +64,7 @@ int main(int argc, char const *argv[] )
     gtk_widget_show(box1);
 
     box2 = gtk_vbox_new(FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(box1), box2, TRUE, TRUE, 0);
     gtk_container_add(GTK_CONTAINER(window), box2);
     gtk_widget_show(box2);
 
@@ -66,33 +73,43 @@ int main(int argc, char const *argv[] )
     gtk_box_pack_start(GTK_BOX(box2), button, TRUE, TRUE, 0);
     gtk_widget_show(button);
 
-    g_signal_connect(button1, "released", G_CALLBACK(release_callback),(gpointer) "button");
-    g_signal_connect(button1, "clicked", G_CALLBACK(click_callback),(gpointer) "button");
-    g_signal_connect(button1, "toggled", G_CALLBACK(toggled_callback),(gpointer) "button");
-    g_signal_connect(button1, "pressed", G_CALLBACK(press_callback),(gpointer) "button");
-    g_signal_connect(button1, "enter", G_CALLBACK(enter_callback),(gpointer) "button");
-    g_signal_connect(button1, "leave", G_CALLBACK(leave_callback),(gpointer) "button");
+    //g_signal_connect(button, "released", G_CALLBACK(release_callback),(gpointer) "button");
+    //g_signal_connect(button, "clicked", G_CALLBACK(click_callback),(gpointer) "button");
+    g_signal_connect(button, "released", G_CALLBACK(find_which_button_in_group_activated), NULL);
+    //g_signal_connect(button, "pressed", G_CALLBACK(press_callback),(gpointer) "button");
+    //g_signal_connect(button, "enter", G_CALLBACK(enter_callback),(gpointer) "button");
+    //g_signal_connect(button, "leave", G_CALLBACK(leave_callback),(gpointer) "button");
     group = gtk_radio_button_get_group(GTK_RADIO_BUTTON(button));
-    
+
     //button 2
     button = gtk_radio_button_new_with_label(group, "button 2");
+    g_signal_connect(button, "released", G_CALLBACK(find_which_button_in_group_activated), NULL);
     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(button), TRUE);
     gtk_box_pack_start(GTK_BOX(box2), button, TRUE, TRUE, 0);
     gtk_widget_show(button);
 
     // button 3
-    button = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(button),
-	                                                  "button 3");
+    // gtk_radio_button_new_with_label(gtk_radio_button_get_group (GTK_RADIO_BUTTON (button)), "button 3");
+    button = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(button), "button 3");
+    g_signal_connect(button, "released", G_CALLBACK(find_which_button_in_group_activated), NULL);
     gtk_box_pack_start(GTK_BOX(box2), button, TRUE, TRUE, 0);
     gtk_widget_show(button);
 
-    // Insert button 1 into the upper left quadrant of the table
-    box = xpm_label_box("folder.xpm", "Open file");
+    seperator = gtk_hseparator_new();
+    gtk_box_pack_start(GTK_BOX(box1), button, FALSE, TRUE, 0);
+    gtk_widget_show(seperator);
 
-    gtk_widget_show(box);
+    box2 = gtk_vbox_new(FALSE, 10);
+    gtk_container_set_border_width(GTK_CONTAINER(box2), 10);
+    gtk_box_pack_start(GTK_BOX(box1), box2, TRUE, TRUE, 0);
+    gtk_widget_show(box2);
 
-    gtk_container_add(GTK_CONTAINER(button), box);
-    gtk_container_add(GTK_CONTAINER(window), button);
+    button = gtk_button_new_with_label("close");
+    g_signal_connect(button, "clicked", gtk_main_quit, NULL);
+    gtk_box_pack_start(GTK_BOX(box2), button, TRUE, TRUE, 0);
+    gtk_widget_set_can_default(button, TRUE);
+    gtk_widget_grab_default(button);
+    gtk_widget_show(button);
 
     gtk_widget_show(window);
 
